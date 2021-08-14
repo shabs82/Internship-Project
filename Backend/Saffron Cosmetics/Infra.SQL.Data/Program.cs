d0e0909b98930ab73c60ed1db6035b11a90ec1a3
shabs <43846967+shabs82@users.noreply.github.com>
2021-08-14 12:19:57 +0200

    Backend Architecture

---
+++ Backend/Saffron Cosmetics/Infra.SQL.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infra.SQL.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Backend/Saffron Cosmetics/SafCos.Core.Entities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafCos.Core.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
