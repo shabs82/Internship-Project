61cc820330e9f89c880314e83dc55aa5af9a5606
shabs <43846967+shabs82@users.noreply.github.com>
2021-08-15 21:44:39 +0200

    Entities
    Entities Created

---
+++ Backend/Saffron Cosmetics/SafCos.Core.Entities/Face/CompactPowder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafCos.Core.Entities.Face
+{
+    public class CompactPowder
+    {
+        public int Id { get; set; }
+        public int ProductCode { get; set; }
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string Brand { get; set; }
+        public int Availability { get; set; }
+        public string Description { get; set; }
+    }
+}
