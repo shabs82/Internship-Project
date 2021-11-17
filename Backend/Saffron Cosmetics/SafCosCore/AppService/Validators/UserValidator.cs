931fe2316ed817f09ed5cabb73c4b6c3e60117ae
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-11-17 21:06:57 +0100

    Changes

--- Backend/Saffron Cosmetics/SafCosCore/AppService/Validators/UserValidator.cs
+++ Backend/Saffron Cosmetics/SafCosCore/AppService/Validators/UserValidator.cs
@@ -8,18 +8,20 @@ using System.Threading.Tasks;
 
 namespace SafCos.Core.AppService.Validators
 {
-    public class UserValidator : AbstractValidator<User>
+    public class UserValidator : AbstractValidator<LoginInputModel>
     {
         public UserValidator()
         {
-            RuleFor(p => p.Email).MaximumLength(50).WithMessage("name cannot be more than 50 characters");
-            RuleFor(p => p.Email).MinimumLength(5).WithMessage("name cannot be less than 5 characters");
-            RuleFor(p => p.Email).NotNull().WithMessage("username cannot be empty");
-            RuleFor(p => p.Email).Null().WithMessage("username already taken");
-            RuleFor(p => p.Email).Matches("(**?:[^​˜!#$]*[˜​!#$]**)").WithMessage("special characters not allowed in username");
+            RuleFor(p => p.Username).MaximumLength(50).WithMessage("name cannot be more than 50 characters");
+            RuleFor(p => p.Username).MinimumLength(5).WithMessage("name cannot be less than 5 characters");
+            RuleFor(p => p.Username).NotNull().WithMessage("username cannot be empty");
+            RuleFor(p => p.Username).Null().WithMessage("username already taken");
+            //RuleFor(p => p.Username).Matches("(**?:[^​˜!#$]*[˜​!#$]**)").WithMessage("special characters not allowed in username");
+            RuleFor(p => p.Password).MinimumLength(3).WithMessage("password cannot be less than 5 characters");
+            RuleFor(p => p.Password).NotNull().WithMessage("password cannot be empty");
             
-            
+
         }
-       
+
     }
 }
